0ed18ca6fedeceb43afcf32a40c8f8112d836f70
Akshi4162 <akshi1246@gmail.com>
2024-05-10 21:06:11 +0800

    Updated game module & Test

---
+++ Akshitha_GameUse.js
@@ -0,0 +1,111 @@
+let playerProgress = {
+    level: 1,
+    experience: 0,
+    coins: 100,
+    inventory: [],
+    unlockedSkills: []
+};
+
+let shopItems = [
+    { name: "Boiled eggs", price: 20 },
+    { name: "Fairy", price: 50 },
+    { name: "Legit Master Sword", price: 100000 } 
+];
+
+let availableSkills = ["Protection", "Heal", "Fly", "Thunder"];
+
+module.exports = {
+    // Display the number of coins the player currently has
+    checkCoins() {
+        return `Coins left : ${playerProgress.coins} `;
+    },
+
+    // Check shop items
+    checkShopItems() {
+        let itemsString = shopItems.map(item => `Name: ${item.name}, Price: ${item.price}`).join("\n");
+        return ` Items available to buy: \n${itemsString}`;
+    },
+
+    // Allow the player to spend coins to purchase items from a shop
+    buyItem(itemIndex) {
+        let item = shopItems[itemIndex];
+        if (playerProgress.coins >= item.price) {
+            playerProgress.coins -= item.price;
+            playerProgress.inventory.push(item.name);
+            return `Bought ${item.name} for ${item.price} coins.`;
+        } else {
+            return `You don't have enough coins to buy this ${item.name}.`;
+        }
+    },
+
+    // Allow the player to sell items to gain some coins
+    sellItem(itemIndex) {
+        if (itemIndex < 0 || itemIndex >= playerProgress.inventory.length) {
+            return "There's no items you can sell.";
+        }
+
+        let item = playerProgress.inventory[itemIndex];
+        let itemInShop = shopItems.find(shopItem => shopItem.name === item);
+
+        if (itemInShop) {
+            let sellingPrice = Math.floor(itemInShop.price * 0.5); 
+            playerProgress.coins += sellingPrice;
+            playerProgress.inventory.splice(itemIndex, 1); 
+            return `Sold ${item} for ${sellingPrice} coins.`;
+        } else {
+            return `Item ${item} is not sellable.`;
+        }
+    },
+
+    // Display the items the player currently owns
+    viewInventory() {
+        if (playerProgress.inventory.length === 0) {
+            return "Inventory : None";
+        } else {
+            return `Inventory : ${playerProgress.inventory}`;
+        }
+    },
+
+    getUnlockedSkills(){
+        if (playerProgress.unlockedSkills.length === 0){
+            return "Unlocked Skills: None";
+        } else{
+        return `All skills you unlocked: ${playerProgress.unlockedSkills}`;
+        }
+    },
+
+    // Allow the player to unlock new skills
+    unlockSkill(skillIndex) {
+        let skill = availableSkills[skillIndex];
+        if (!playerProgress.unlockedSkills.includes(skill)) {
+            playerProgress.unlockedSkills.push(skill);
+            return `You unlocked ${skill}.`;
+        } else {
+            return `${skill} is already unlocked.`;
+        }
+    },
+
+    //getplayer's level
+    getPlayerLevel(){
+        return `Current Level: ${playerProgress.level}`;
+    },
+
+    getPlayerExperience(){
+        return `Experience Points in your current level (${playerProgress.level}): ${playerProgress.experience}`;
+    },
+ 
+    // Increase the player's experience points based on their actions in the game
+    addExperience(points) {
+        playerProgress.experience += points;
+        if (playerProgress.experience >= 500) {
+            let levelIncrease = Math.floor(playerProgress.experience / 500); // Calculate level increase
+            playerProgress.level += levelIncrease; // Increase player's level
+            playerProgress.experience %= 500; // Reset experience points
+            // return `You gained ${points} experience points.`;
+            return `You gained ${points} experience points. \nCongrats, you leveled up! Your new level is ${playerProgress.level}.`;
+        } else {
+            return `You gained ${points} experience points.`;
+        }
+    },
+
+};
---
+++ game_test.js
@@ -0,0 +1,46 @@
+const gameModule = require('./Akshitha_GameUse.js'); 
+
+
+// CHECKING PLAYER'S INITIAL STATE
+console.log("Initial player's state:");
+console.log(gameModule.getPlayerLevel()); 
+console.log(gameModule.getPlayerExperience()); 
+console.log(gameModule.getUnlockedSkills());
+console.log(gameModule.checkCoins());
+console.log(gameModule.viewInventory()); 
+
+// TRYING TO UNLOCK SKILLS 
+console.log("\nUnlocking Various Skills:");
+console.log(gameModule.unlockSkill(0)); 
+console.log(gameModule.unlockSkill(0)); // If try to unlock the same skill again
+console.log(gameModule.unlockSkill(1));
+
+console.log(gameModule.getUnlockedSkills());
+
+//CHECKING ALL THE AVAILABLE ITEMS IN SHOP
+console.log("\nCheck all available items on sale: ");
+console.log(gameModule.checkShopItems()); 
+
+//TRYING TO BUY ITEM(S)
+console.log("\nBuying an item:");
+console.log(gameModule.buyItem(1)); 
+console.log(gameModule.checkCoins()); // Check updated number of coins
+console.log(gameModule.viewInventory()); // Check updated inventory
+
+//TRYING TO SELL ITEM(S)
+console.log("\nSelling an item:");
+console.log(gameModule.sellItem(0)); 
+console.log(gameModule.checkCoins()); 
+console.log(gameModule.viewInventory()); 
+
+//ADDING GAME EXPERIENCE POINTS TO INCREASE PLAYER'S LEVEL
+console.log("\nAdding game experience points to increase level :");
+console.log(gameModule.addExperience(50));
+console.log(gameModule.getPlayerExperience());
+console.log(gameModule.addExperience(470));
+console.log(gameModule.getPlayerExperience());
+console.log(gameModule.getPlayerLevel());
+
+
+
+
